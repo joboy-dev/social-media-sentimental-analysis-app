adeae34aff605283efe7692eef59bc4e310d74db
Adegbehingbe Oluwakorede Joseph <oluwakoredeadegbehingbe@gmail.com>
2025-03-18 01:46:29 +0100

    chore: add firebase config

---
+++ utils/firebase_config.py
@@ -0,0 +1,11 @@
+firebase_config = {
+  'apiKey': "AIzaSyCUzTFWjsDW816NT8JgNQiCMgmGwUxkGj0",
+  'authDomain': "python-storage-d1e10.firebaseapp.com",
+  'projectId': "python-storage-d1e10",
+  'storageBucket': "python-storage-d1e10.appspot.com",
+  'messagingSenderId': "253588525093",
+  'appId': "1:253588525093:web:b983734a4caf1ea32c6fcc",
+  'measurementId': "G-VPQSJXB8W5",
+  'serviceAccount': 'utils/serviceAccount.json',
+  'databaseURL': 'https://python-storage-d1e10-default-rtdb.firebaseio.com/'
+}
